ff95d7eed03f8b00307506dbaa4e1c331525e0d9
famous <famouspaul56@gmail.com>
2024-08-13 03:02:21 -0700

    bigfamous

---
+++ src/app/project/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <section>
+        <div>
+            <h1>Heading</h1>
+            <h1>Heading</h1>
+            <h1>Heading</h1>
+            <h1>Heading</h1>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default page
